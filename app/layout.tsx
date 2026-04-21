06cd22fe21620ffdf58200d43ed0f048f2c0cdb8
Emerson Domingo Jr <263358540+emdomingojr@users.noreply.github.com>
2026-04-21 20:25:53 +1000

    Feat(Analytics): Add Hotjar tracking script to layout head

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,6 +66,19 @@ export default function RootLayout({
             __html: `(function(){try{var t=localStorage.getItem('theme');document.documentElement.setAttribute('data-theme',t==='dark'?'dark':'light');}catch(e){}})();`,
           }}
         />
+        {/* Hotjar Tracking Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:5158819,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+          }}
+        />
       </head>
       <body className="font-sans">
         <DevModeProvider>
